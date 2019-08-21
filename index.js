561f243930bfff970161c8f0c4fdef129e526b31
Ruben Hakopian <ruben.hakopian@gmail.com>
2019-08-20 17:56:11 -0700

    added userinfo query api.

--- index.js
+++ index.js
@@ -3,13 +3,11 @@ const axios = require('axios');
 module.exports.DOMAIN = `https://${process.env.AUTH0_TENANT_NAME}.auth0.com`
 module.exports.AUDIENCE = `${module.exports.DOMAIN}/api/v2/`;
 
-module.exports.client = function(event) {
-    var headers = {};
+module.exports.client = function(event, extraHeaders) {
+    var headers = extraHeaders || {};
     var origIP = getOrigIP(event);
     if (origIP) {
         headers['auth0-forwarded-for'] = origIP;
-    } else {
-
     }
     const instance = axios.create({
         headers: headers
@@ -17,10 +15,34 @@ module.exports.client = function(event) {
     return instance;
 }
 
+module.exports.clientAuth = function(event) {
+    var extraHeaders = {};
+    if (event.headers.Authorization) {
+        extraHeaders.Authorization =  `Bearer ${event.headers.Authorization}`;
+    }
+    return module.exports.client(event, extraHeaders)
+}
+
 module.exports.post = function(event, url, postData) {
     var client = module.exports.client(event);
     var fullUrl = `${module.exports.DOMAIN}${url}`;
     return client.post(fullUrl, postData);
+}
+
+module.exports.getAuth = function(event, url) {
+    var client = module.exports.clientAuth(event);
+    var fullUrl = `${module.exports.DOMAIN}${url}`;
+    return client.get(fullUrl);
+}
+
+module.exports.postAuth = function(event, url, postData) {
+    var client = module.exports.clientAuth(event);
+    var fullUrl = `${module.exports.DOMAIN}${url}`;
+    return client.post(fullUrl, postData);
+}
+
+module.exports.getUserInfo = function(event) {
+    return module.exports.getAuth(event, '/userinfo');
 }
 
 function getOrigIP(event)
